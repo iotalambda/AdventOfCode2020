c1f89294c710b35a8c3ae9998544d6b921421db7
Joona Luoma <joona.luoma@hotmail.com>
2020-12-28 18:24:48 +0200

    Add D1.C1

--- D1/C1/Program.fs
+++ D1/C1/Program.fs
@@ -1,7 +1,17 @@
 open System
 open Utils
+open Utils.Functional
+open Utils.String
+open Utils.Pair
+
+let findPair = (both Seq.allPairs) >> (flip Seq.find) |> flip
 
 [<EntryPoint; STAThread>]
-let main argv =
+let main _ =
+    Clipboard.get()
+    |> linesToIntSeq
+    |> findPair (sum >> (=)2020)
+    |> multiply
+    |> string
+    |> printfn " ##### Result %s"
     0
-    
--- Utils/Library.fs
+++ Utils/Library.fs
@@ -6,3 +6,42 @@ module Clipboard =
     let get () = ClipboardService.GetText()
     let set = ClipboardService.SetText
     let apply f = get () |> f |> set
+
+module String =
+    let newline = System.Environment.NewLine
+
+    let toSeq (separator: string) converter (source: string) =
+        source.Split([| separator |], System.StringSplitOptions.RemoveEmptyEntries)
+        |> Seq.map (converter)
+
+    let split s = toSeq s id >> Seq.toList
+    let linesToSeq converter = toSeq newline converter
+    let linesToStringSeq = linesToSeq id
+    let linesToStringList = linesToStringSeq >> Seq.toList
+    let linesToIntSeq = linesToSeq int
+    let csvToStringSeq = toSeq "," id
+    let csvToStringList = csvToStringSeq >> Seq.toList
+    let csvToIntSeq = toSeq "," int
+    let csvToIntList = csvToIntSeq >> Seq.toList
+
+module List =
+    let replace<'T> ix (sub: 'T) =
+        List.mapi (fun ix0 x -> if ix0 = ix then sub else x)
+
+    let slice ix1 ix2 list = list |> List.skip ix1 |> List.take ix2
+
+module Seq =
+    let collecti f = Seq.mapi f >> Seq.collect id
+    let choosei f = Seq.mapi f >> Seq.choose id
+    let all f = Seq.exists (f >> (not)) >> (not)
+
+module Pair =
+    let map f (a, b) = (f a, f b)
+    let sum (a, b) = a + b
+    let multiply (a, b) = a * b
+
+module Functional =
+    let flip f a b = f b a
+    let thd (a, b, c) = c
+    let fth (a, b, c, d) = d
+    let both f a = f a a
