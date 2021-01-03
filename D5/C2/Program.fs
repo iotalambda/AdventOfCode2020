a3c207955999a8fbe97c980a2cc87bbf444617f5
Joona Luoma <joona.luoma@hotmail.com>
2021-01-03 06:07:47 +0200

    Add D5 C2

---
+++ D5/C2/Program.fs
@@ -0,0 +1,30 @@
+open System
+open Utils
+
+let getSeatId =
+    String.replace "F" "0"
+    >> String.replace "B" "1"
+    >> String.replace "R" "1"
+    >> String.replace "L" "0"
+    >> fun s -> Convert.ToInt64(s, 2)
+    >> int
+
+[<EntryPoint; STAThread>]
+let main _ =
+
+    // Get and wrangle data
+    Clipboard.get () // fsharplint:disable-line Hints
+    |> String.linesToStringList
+
+    // Do the thing
+    |> Seq.map getSeatId
+    |> Seq.sort
+    |> Seq.pairwise
+    |> Seq.find (fun (l, r) -> r = l + 2)
+    |> fst
+    |> (+) 1
+
+    // Output result
+    |> printfn " ##### Result %d"
+
+    0
